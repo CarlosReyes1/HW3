615182680f1e15aa6d8ff0bc89ed08a3c61dfe3d
CarlosReyes1 <reyes_carlos1@yahoo.com>
2016-02-01 09:34:25 -0800

    Update ex7.go

--- ex7.go
+++ ex7.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	sum := 0
 
-	for i := 0; i <= 1000; i++ {
+	for i := 0; i < 1000; i++ {
 		if i%3 == 0 {
 			sum += i
 		} else if i%5 == 0 {
@@ -15,3 +15,4 @@ func main() {
 
 	fmt.Println(sum)
 }
+//the answer to the problem number 1 in projecteuler.net is 233168
